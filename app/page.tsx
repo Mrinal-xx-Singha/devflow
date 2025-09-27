2e66ffe6b72a9048ccb0435f9ecf531947f6978a
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2025-09-27 23:05:06 +0530

    font setup linter preetier and custom tailwind class setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 const App = () => {
- 
   return (
     <div>
-      <h1 className="text-3xl ">Welcome to Next.js</h1>
+      <h1 className="h1-bold font-inter text-light-400">
+        Welcome to Next.js Inter Font
+      </h1>
+      <h1 className="h1-bold font-space-grotesk">Welcome to Next.js Grotesk</h1>
     </div>
   );
 };

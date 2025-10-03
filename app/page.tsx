fdf186163517652b4f2b04c5499e41edc2047ecb
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2025-10-03 18:52:21 +0530

    clean up

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
-import Image from "next/image";
-import React from "react";
+import React from 'react'
 
-const App = () => {
+const Home = () => {
   return (
-    <div>
-      <h1 className="h1-bold font-inter text-light-400">
-        Welcome to Next.js Inter Font
-      </h1>
-      <h1 className="h1-bold font-space-grotesk">Welcome to Next.js Grotesk</h1>
-      <Image src="/images/logo.png" alt="Site Logo" width={100} height={100} />
-    </div>
-  );
-};
+    <div>Refactor</div>
+  )
+}
 
-export default App;
+export default Home

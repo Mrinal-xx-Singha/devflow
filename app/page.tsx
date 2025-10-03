537aceee2649ae14fb15ff94bd721a2b61d2c040
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2025-10-03 22:24:39 +0530

    font setup and theme setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const Home = () => {
   return (
-    <div>Refactor</div>
-  )
-}
+    <div>
+      <h1 className="h1-bold font-inter text-light-400">
+        Welcome to Next.js Inter Font
+      </h1>
+      <h1 className="h1-bold font-space-grotesk">Welcome to Next.js Grotesk</h1>
+      <Image src="/images/logo.png" alt="Site Logo" width={100} height={100} />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
